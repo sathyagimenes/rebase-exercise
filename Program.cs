bbd9f6c08999eccc0ce1daa11552236c53d48373
sathyagimenes <sathya.gimenes@gmail.com>
2023-06-02 19:44:24 -0300

    V1.0

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int number = 0;
+        for (int i = 1; i <= 1000; i++)
+        {
+            if (i % 7 == 0 && i % 13 == 0 && i % 17 == 0)
+            {
+                number = i;
+                break;
+            }
+        }
+
+        if (number != 0)
+        {
+            Console.WriteLine("O primeiro número divisível por 7, 13 e 17 é: " + number);
+        }
+        else
+        {
+            Console.WriteLine("Nenhum número divisível por 7, 13 e 17 encontrado entre 1 e 1000.");
+        }
+    }
+}
